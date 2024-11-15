381397374ab6c6e7e9aed2cfbadc55abb37141cd
bitpaus7 <bitpaus@gmail.com>
2024-11-15 19:38:35 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Set interval for sliding every 5 seconds
+setInterval(nextSlide, 5000);
+
+// Show the first slide on load
+showSlide(currentSlide);
